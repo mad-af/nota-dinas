becd1f1512cb9cd460b4d06f95c2bf52febf0de0
Mochamad Aufa <mochamadaufa7@gmail.com>
2025-12-02 13:59:37 +0700

    style(NotaDinas): change document container bg from white to amber-100

--- resources/js/Pages/NotaDinas/Lampiran/Sign.vue
+++ resources/js/Pages/NotaDinas/Lampiran/Sign.vue
@@ -353,7 +353,7 @@ onUnmounted(() => {
         </div>
 
         <div class="flex flex-col gap-6 md:flex-row">
-          <div class="p-0 bg-white shadow-sm sm:rounded-lg md:p-6 md:flex-1">
+          <div class="p-0 bg-amber-100 shadow-sm sm:rounded-lg md:p-6 md:flex-1">
             <div class="flex flex-col gap-4 px-6 pt-6 md:flex-row md:items-center md:justify-between md:p-0">
               <h2 class="text-lg font-semibold text-gray-800 sm:text-xl">Dokumen</h2>
               <div class="flex flex-wrap gap-2 items-center">
